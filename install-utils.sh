30ed75fa410efbe02259458d109b511dc2d508d9
Mika Mäki <mika.maki@vexlum.com>
2025-01-04 11:58:03 +0200

    Add git-http to utils

--- install-utils.sh
+++ install-utils.sh
@@ -2,7 +2,7 @@
 set -eu
 
 apk update
-apk add git librespeed-go luci luci-ssl luci-app-attendedsysupgrade luci-app-snmpd luci-proto-wireguard nano
+apk add git git-http librespeed-go luci luci-ssl luci-app-attendedsysupgrade luci-app-snmpd luci-proto-wireguard nano
 
 # Enable the LibreSpeed service here
 # nano /etc/config/librespeed-go
